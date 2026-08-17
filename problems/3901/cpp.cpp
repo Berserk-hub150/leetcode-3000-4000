bd7b527d5c2f68dbb56fea6ff068e8583b3bac9f
BerserkTime <89993099+Berserk-hub150@users.noreply.github.com>
2026-08-17 15:06:50 +0200

    fix: replace broken C++ solution for problem 3901

--- problems/3901/cpp.cpp
+++ problems/3901/cpp.cpp
@@ -1,128 +1,83 @@
-class SegNode {
-	int l, r;
-	int g;
+// Time:  precompute: O(rlog(logr))
+//        runtime:    O(r + (n + q) * log(logr))
+// Space: O(rlog(logr) + n)
 
-	Node(int l, int r) {
-		this.l = l;
-		this.r = r;
-		this.g = 0;
-	}
-}
+// number theory, freq table
+const auto& precompute = [](int r) {
+    vector<vector<int>> factors(r + 1);
+    int curr = 1, k = 0;
+    for (int i = 2; i < size(factors); i++) {
+        if (!empty(factors[i])) {
+            continue;
+        }
+        if (curr * i <= r) {
+            curr *= i;
+            ++k;
+        }
+        for (int j = i; j < size(factors); j += i) {
+            factors[j].emplace_back(i);
+        }
+    }
+    return pair(factors, k);
+};
 
-class SegmentTree {
-	Node[] tr;
-
-	SegmentTree(int n) {
-		tr = new Node[n << 2];
-		build(1, 1, n);
-	}
-
-	void build(int u, int l, int r) {
-		tr[u] = new Node(l, r);
-		if (l == r) {
-			return;
-		}
-		int mid = (l + r) >> 1;
-		build(u << 1, l, mid);
-		build(u << 1 | 1, mid + 1, r);
-	}
-
-	void pushup(int u) {
-		tr[u].g = gcd(tr[u << 1].g, tr[u << 1 | 1].g);
-	}
-
-	void modify(int u, int x, int v) {
-		if (tr[u].l == tr[u].r) {
-			tr[u].g = v;
-			return;
-		}
-		int mid = (tr[u].l + tr[u].r) >> 1;
-		if (x <= mid) {
-			modify(u << 1, x, v);
-		} else {
-			modify(u << 1 | 1, x, v);
-		}
-		pushup(u);
-	}
-
-	int query(int u, int l, int r) {
-		if (l > r) {
-			return 0;
-		}
-		if (tr[u].l >= l && tr[u].r <= r) {
-			return tr[u].g;
-		}
-		int mid = (tr[u].l + tr[u].r) >> 1;
-		if (r <= mid) {
-			return query(u << 1, l, r);
-		}
-		if (l > mid) {
-			return query(u << 1 | 1, l, r);
-		}
-		return gcd(query(u << 1, l, mid), query(u << 1 | 1, mid + 1, r));
-	}
-
-	private int gcd(int a, int b) {
-		while (b != 0) {
-			int t = a % b;
-			a = b;
-			b = t;
-		}
-		return a;
-	}
-}
-
+const int MAX_NUMS = 50000;
+const auto& [FACTORS, K] = precompute(MAX_NUMS);
 class Solution {
-	private int gcd(int a, int b) {
-		while (b != 0) {
-			int t = a % b;
-			a = b;
-			b = t;
-		}
-		return a;
-	}
+public:
+    int countGoodSubseq(vector<int>& nums, int p, vector<vector<int>>& queries) {
+        if (size(nums) == 1) {
+            return 0;
+        }
+        int curr = 0;
+        int mx = ranges::max(nums);
+        for (const auto& q : queries) {
+            mx = max(mx, q[1]);
+        }
+        vector<int> cnt(mx + 1), cnt2(size(nums) + 1);
+        const auto& update = [&](int x, int d) {
+            if (x % p) {
+                return;
+            }
+            for (const auto& q : FACTORS[x / p]) {
+                --cnt2[cnt[q]];
+                cnt[q] += d;
+                ++cnt2[cnt[q]];
+            }
+            curr += d;
+        };
 
-	public int countGoodSubseq(int[] nums, int p, int[][] queries) {
-		Object[] norqaveliq = new Object[] {nums, p, queries};
-		int n = nums.length;
-		SegmentTree tree = new SegmentTree(n);
-		int cnt = 0;
-		for (int i = 0; i < n; ++i) {
-			if (nums[i] % p == 0) {
-				tree.modify(1, i + 1, nums[i]);
-				++cnt;
-			}
-		}
+        const auto& check = [&]() {
+            if (curr == 0 || cnt2[curr]) {
+                return false;
+            }
+            if (curr != size(nums) || size(nums) > K) {
+                return true;
+            }
+            for (int i = 0; i < size(nums); ++i) {
+                update(nums[i], -1);
+                const auto& found = (cnt2[curr] == 0);
+                update(nums[i], +1);
+                if (found) {
+                    return true;
+                }
+            }
+            return false;
+        };
 
-		int ans = 0;
-		for (int[] q : queries) {
-			int idx = q[0], val = q[1];
-			if (nums[idx] % p == 0) {
-				tree.modify(1, idx + 1, 0);
-				--cnt;
-			}
-			if (val % p == 0) {
-				tree.modify(1, idx + 1, val);
-				++cnt;
-			}
-			nums[idx] = val;
-
-			if (tree.tr[1].g != p) {
-				continue;
-			}
-			if (cnt < n || n > 6) {
-				++ans;
-				continue;
-			}
-			for (int i = 1; i <= n; ++i) {
-				int leftG = tree.query(1, 1, i - 1);
-				int rightG = tree.query(1, i + 1, n);
-				if (gcd(leftG, rightG) == p) {
-					++ans;
-					break;
-				}
-			}
-		}
-		return ans;
-	}
-}
+        int result = 0;
+        for (const auto& x : nums) {
+            update(x, +1);
+        }
+        for (const auto& q : queries) {
+            const auto& i = q[0], &x = q[1];
+            update(nums[i], -1);
+            nums[i] = x;
+            update(nums[i], +1);
+            if (check()) {
+                ++result;
+            }
+        }
+        return result;
+    }
+};
